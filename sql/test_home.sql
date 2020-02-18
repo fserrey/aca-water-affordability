b00e7b7d5babc894e601d06a9609fd51e9301751
fserrey <fserrey@gmail.com>
2020-02-18 23:50:46 +0100

    new tables uploaded

---
+++ sql/test_home.sql
@@ -0,0 +1,25 @@
+USE aca_epf;
+
+SELECT COUNT(distinct id) FROM hogar_ine
+where impexac <> ''
+and IMPEXAC is not null;
+
+
+SELECT COUNT(*) FROM home_ine;
+where impexacpsp <> ''
+and IMPEXACPSP is not null;
+
+###########AÑADIENDO NUEVA COLUMNA DE TABLA MADRE#################
+ALTER TABLE home_ine ADD COLUMN IMPEXAC INTEGER UNSIGNED DEFAULT 0;
+UPDATE home_ine t1 
+INNER JOIN hogar_ine t2 ON t1.id_d = t2.id_d 
+SET t1.IMPEXAC = t2.IMPEXAC;
+############################
+
+
+SELECT gasto.ANOENC,gasto.CODIGO, SUM(IF(((gasto.GASTO/12)/hogar.IMPEXAC)*100 > 5, 1,0)) FLAG
+FROM aca_epf.gastos_ine gasto
+LEFT JOIN (SELECT home_ine.id_d ,home_ine.ANOENC, home_ine.IMPEXAC 
+			FROM hogar_ine ) hogar ON gasto.id_d = hogar.id_d
+WHERE CODIGO between '04511' and '04551'
+group by 1,2;
